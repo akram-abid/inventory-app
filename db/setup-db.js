fc4beaf1e0729e3b37074b2b60f8f91fe1977f5b
akram-abid <a.abid44778@gmail.com>
2025-05-31 18:00:28 +0100

    dynamicly set the connection link

--- db/setup-db.js
+++ db/setup-db.js
@@ -79,7 +79,7 @@ VALUES
 
   console.log("Creating tables and seeding data...");
   const client = new Client({
-    connectionString: process.env.DATABASE_URL + "?sslmode=require",
+    connectionString: "postgresql://akr4m:shoyo@localhost:5432/inventory_app",
   });
   
   try {
