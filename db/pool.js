6f35d30439eb0b9e90d6c0d54e7d34ba87c5521a
AKRAM ABID <102604276+akram-abid@users.noreply.github.com>
2025-05-31 18:26:19 +0100

    Update pool.js
    trying to connect using connection string instead of DB details

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,16 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({
+/* module.exports = new Pool({
     host: "localhost",
     user: process.env.USER,
     database: process.env.DB_NAME,
     password: process.env.PASSWORD,
     port: 5432
+});*/
+
+module.exports = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, 
+  },
 });
